7708a9722eccf4a965e2572cc933b91f293c3b3d
rishabh <therishabhrox@gmail.com>
2024-02-05 23:17:49 +0530

    static keyword

--- oops.java
+++ oops.java
@@ -1,15 +1,35 @@
 
 class Calculator {
     
-    int a;
+    int a; // instance variable
+    static int b; // static variable
     
+    public Calculator() {
+        a = 0;
+    }
+
+    // static block
+    static {
+        b = 1;
+    }
+
     public int add(int num1, int num2) {
+        
+        int c; // local variable
         return num1+num2;
+    }
+
+    // static method
+    public static int sub(int num1, int num2) {
+        return num1-num2;
     }
 }
 
 public class oops {
     public static void main(String[] args) {
+        
+        // Class.forName("Calculator");
+        
         int n1=2, n2=2;
         Calculator c1 = new Calculator();
         
