78c9b45cc97dad5046c03602f5b933559cfbfe46
rishabh <therishabhrox@gmail.com>
2024-02-06 00:08:59 +0530

    encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,29 @@
+
+class Human {
+    
+    // instance variables are private so that they cannot be accessed directly from outside the class
+    private int age;
+    private String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        
+    }
+}
