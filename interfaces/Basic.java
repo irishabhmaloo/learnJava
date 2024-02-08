80358208ad1566507eaab20be83c092e0f73238b
rishabh <therishabhrox@gmail.com>
2024-02-08 21:12:29 +0530

    interfaces

---
+++ interfaces/Basic.java
@@ -0,0 +1,35 @@
+package interfaces;
+
+interface A {
+    // all variable, by default: finala nd static
+    int age=64;
+    String s="hello";
+
+    // all methods, by default: public and abstract
+    void show();
+    public abstract void config();
+}
+
+
+// interface C extends A{
+    
+// }
+
+
+class B implements A {
+    public void show() {
+        System.out.println("Show");
+    }
+
+    public void config() {
+        System.out.println("Config");
+    }
+}
+
+public class Basic {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.show();
+        obj.config();
+    }
+}
