119311c890d2522eeabf7d42c5e2056c72a25b53
rishabh <therishabhrox@gmail.com>
2024-03-06 21:58:29 +0530

    Spring example - 06/03/24

--- spring/demo/Main.java
+++ spring/demo/Main.java
@@ -1,13 +1,16 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     
     public static void main(String[] args) {
         
-        Staff doctor = new Doctor();
-        Staff nurse = new Nurse();
-        
+        // this is the context
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class); 
+
+        Staff doctor = new Doctor(); // this is the bean
         doctor.work();
-        nurse.work();
     }
 }
